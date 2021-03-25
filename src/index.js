e372e79850a2780fc71abf64a92be38099e00eb6
Mabilog <g.angelo.m@gmail.com>
2021-03-25 07:55:42 -0400

    css done?

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log("heeeeelllloooo world!");
+const menu = document.querySelector('#mobile-menu');
+const menuLink = document.querySelector('.navbar__menu');
+
+menu.addEventListener('click', function () {
+  menu.classList.toggle('is-active');
+  menuLink.classList.toggle('active');
+})
