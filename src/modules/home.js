ad98946604f7af82871edcace8bd0de079b5b6be
angelo-mab <60443712+angelo-mab@users.noreply.github.com>
2021-04-01 15:43:47 -0400

    made the font color in menu container white just to stop the problem warning lool

--- src/modules/home.js
+++ src/modules/home.js
@@ -3,10 +3,10 @@ const home = (() => {
   const home = document.createElement('div');
   home.setAttribute('class', 'main');
   home.innerHTML = `
-    <div class="main__container">
+    <div class="main__container" data-tab-content >
       <div class="main__content">
-        <h1>NEXT GENERATION</h1>
-        <h2>TECHNOLOGY</h2>
+        <h2>NEXT GENERATION</h2>
+        <h3>TECHNOLOGY</h3>
         <p>See what makes us different</p>
         <button class="main__btn"><a href="/index.html">Get Started</a></button>
       </div>
